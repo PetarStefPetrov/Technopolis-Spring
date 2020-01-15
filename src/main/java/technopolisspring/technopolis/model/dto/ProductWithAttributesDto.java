952b37db72e41919b195d514faaa20e2a6fca5e6
Petar Petrov <petar.stef.petrov@gmail.com>
2020-01-15 18:35:44 +0200

    added equals, hashcode and toString to the two ProductDtos I've missied it on

--- src/main/java/technopolisspring/technopolis/model/dto/ProductWithAttributesDto.java
+++ src/main/java/technopolisspring/technopolis/model/dto/ProductWithAttributesDto.java
@@ -9,6 +9,7 @@ import technopolisspring.technopolis.model.pojos.IProductWithAttributes;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -35,4 +36,23 @@ public class ProductWithAttributesDto implements IProductWithAttributes {
         this.offerId = offerId;
         this.attributes = new ArrayList<>();
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductWithAttributesDto that = (ProductWithAttributesDto) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return this.description;
+    }
+
 }
--- src/main/java/technopolisspring/technopolis/model/dto/ProductWithAttributesInOfferDto.java
+++ src/main/java/technopolisspring/technopolis/model/dto/ProductWithAttributesInOfferDto.java
@@ -9,6 +9,7 @@ import technopolisspring.technopolis.model.pojos.IProductWithAttributes;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -37,4 +38,23 @@ public class ProductWithAttributesInOfferDto implements IProductWithAttributes {
         this.offerId = offerId;
         this.attributes = new ArrayList<>();
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductWithAttributesInOfferDto that = (ProductWithAttributesInOfferDto) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return this.description;
+    }
+
 }
